6f81ecc844e8425bc1b096e399af1f208ffcadc0
Rafael Ribeiro <rafael.pr10@gmail.com>
2022-11-07 22:38:14 +0100

    Refactoring Network Manager

--- HomeTestProject/Base/API/NetworkingManager.swift
+++ HomeTestProject/Base/API/NetworkingManager.swift
@@ -12,18 +12,16 @@ final class NetworkingManager {
     
     private init() {}
     
-    func request<T: Codable>(
-        methodType: MethodType = .GET,
-        _ absoluteURL: String,
-        type: T.Type,
-        completion: @escaping (Result<T, Error>) -> Void
+    func request<T: Codable>(_ endpoint: Endpoint,
+                             type: T.Type,
+                             completion: @escaping (Result<T, Error>) -> Void
     ) {
-        guard let url = URL(string: absoluteURL) else {
+        guard let url = endpoint.url else {
             completion(.failure(NetworkingError.invalidUrl))
             return
         }
         
-        let request = buildRequest(from: url, methodType: methodType)
+        let request = buildRequest(from: url, methodType: endpoint.methodType)
 
         let data = URLSession.shared.dataTask(with: request) { data, response, error in
             if error != nil {
@@ -56,17 +54,15 @@ final class NetworkingManager {
         data.resume()
     }
     
-    func request(
-        methodType: MethodType = .GET,
-        _ absoluteURL: String,
-        completion: @escaping (Result<Void, Error>) -> Void
+    func request(_ endpoint: Endpoint,
+                 completion: @escaping (Result<Void, Error>) -> Void
     ) {
-        guard let url = URL(string: absoluteURL) else {
+        guard let url = endpoint.url else {
             completion(.failure(NetworkingError.invalidUrl))
             return
         }
         
-        let request = buildRequest(from: url, methodType: methodType)
+        let request = buildRequest(from: url, methodType: endpoint.methodType)
         
         let data = URLSession.shared.dataTask(with: request) { data, response, error in
             if error != nil {
@@ -115,15 +111,8 @@ extension NetworkingManager.NetworkingError {
     }
 }
 
-extension NetworkingManager {
-    enum MethodType {
-        case GET
-        case POST(data: Data?)
-    }
-}
-
 private extension NetworkingManager {
-    func buildRequest(from url: URL, methodType: MethodType) -> URLRequest {
+    func buildRequest(from url: URL, methodType: Endpoint.MethodType) -> URLRequest {
         var request = URLRequest(url: url)
 
         switch methodType {
