ad8a8db13ebf4490366dce3be804117970016eb5
Rafael Ribeiro <rafael.pr10@gmail.com>
2022-11-09 21:47:14 +0100

    Fix xcodeproj

---
+++ HomeTestProjectTests/Networking/Mocks/UserDetails/NetworkingManagerUserDetailsResponseFailureMock.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingManagerUserDetailsResponseFailureMock.swift
+//  HomeTestProjectTests
+//
+//  Created by Rafael Ribeiro on 09.11.22.
+//
+
+import Foundation
+@testable import HomeTestProject
+
+class NetworkingManagerUserDetailsResponseFailureMock: NetworkingManageable {
+    func request<T>(session: URLSession, _ endpoint: HomeTestProject.Endpoint, type: T.Type) async throws -> T where T : Decodable, T : Encodable {
+        throw NetworkingManager.NetworkingError.invalidUrl
+    }
+    
+    func request(session: URLSession, _ endpoint: HomeTestProject.Endpoint) async throws {}
+}
